0f753b24946139391223f9f0da1d887c2264f189
Rafael Ortiz <rafaelortiz99@outlook.com>
2023-04-27 12:09:53 -0500

    Delete all

--- bases/02-di/src/power/power.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PowerService {
-
-    public supplyPower(watts: number){
-        console.log(`Supplying ${watts} worth of power`);
-    }
-}
